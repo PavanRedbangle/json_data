e0949af8580319253504541d11eef91714b8048b
PavanRedBangle <pavan@redbangle.com>
2023-05-29 17:32:09 +0530

    added json data

---
+++ sample_1.js
@@ -0,0 +1,322 @@
+[
+    {
+        "item": "Receipt of kick off mail + project execution brief",
+        "type": "pre_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":1
+    },
+    {
+        "item": "Final briefing with client servicing + client + writer",
+        "type": "pre_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":2
+    },
+    {
+        "item": "All pending data received from client",
+        "type": "pre_production",
+        "client_action": true,
+        "important_action": false,
+        "heading":false,
+        "order":3
+    },
+    {
+        "item": "Scripting: kick off",
+        "type": "pre_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":4
+    },
+    {
+        "item": "Script, template shortlist and music references: internal review ",
+        "type": "pre_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":5
+    },
+    {
+        "item": "Script, template shortlist and music references: presented / emailed to client",
+        "type": "pre_production",
+        "client_action": true,
+        "important_action": false,
+        "heading":false,
+        "order":6
+    },
+    {
+        "item": "Client sign off / feedback on script, etc",
+        "type": "pre_production",
+        "client_action": true,
+        "important_action": false,
+        "heading":false,
+        "order":7
+    },
+    {
+        "item": "Script revision and finalisation",
+        "type": "pre_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":8
+    },
+    {
+        "item": "Final script: internal review",
+        "type": "pre_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":9
+    },
+    {
+        "item": "Final script: shared with client",
+        "type": "pre_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":10
+    },
+    {
+        "item": "Client sign off on script",
+        "type": "pre_production",
+        "client_action": true,
+        "important_action": false,
+        "heading":false,
+        "order":11
+    },
+    {
+        "item": "Detailed visualisation and stock footage / images shortlist: kick off",
+        "type": "pre_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":12
+    },
+    {
+        "item": "Final script with visual cues: internal review",
+        "type": "pre_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":13
+    },
+    {
+        "item": "Stock music track shortlist: internal review",
+        "type": "pre_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":14
+    },
+    {
+        "item": "Post Production kick offw",
+        "type": "post_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":true,
+        "order":15
+    },
+    {
+        "item": "First 30 seconds of video with music and draft VO: internal review",
+        "type": "post_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":16
+    },
+    {
+        "item": "First draft of master video with music and draft VO (if any): internal review",
+        "type": "post_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":17
+    },
+    {
+        "item": "First draft of master video with music and draft VO (if any): shared with client",
+        "type": "post_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":18
+    },
+    {
+        "item": "(OPTIONAL) VO artist shortlist: internal review",
+        "type": "",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":19
+    },
+    {
+        "item": "(OPTIONAL) VO artist shortlist: shared with client",
+        "type": "post_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":20
+    },
+    {
+        "item": "Client review of first full draft of master video and VO artist shortlist (if required)",
+        "type": "post_production",
+        "client_action": true,
+        "important_action": false,
+        "heading":false,
+        "order":21
+    },
+    {
+        "item": "(OPTIONAL) VO recording and integration",
+        "type": "",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":22
+    },
+    {
+        "item": "(OPTIONAL) Subtitling and integration",
+        "type": "",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":23
+    },
+    {
+        "item": "Final draft of master video with music and professional VO (if any): internal review",
+        "type": "post_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":24
+    },
+    {
+        "item": "Final draft of master video with music and professional VO (if any): shared with client",
+        "type": "post_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":25
+    },
+    {
+        "item": "Client review of final draft master video",
+        "type": "post_production",
+        "client_action": true,
+        "important_action": false,
+        "heading":false,
+        "order":26
+    },
+    {
+        "item": "Procurement of stock music + stock images/ clips",
+        "type": "post_production",
+        "client_action": true,
+        "important_action": false,
+        "heading":false,
+        "order":27
+    },
+    {
+        "item": "Integration of final assets and final output: internal review",
+        "type": "post_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":28
+    },
+    {
+        "item": "Handover of complete film in HD format: master video",
+        "type": "post_production",
+        "client_action": false,
+        "important_action": false,
+        "heading":true,
+        "order":29
+    },
+    {
+        "item": "Script for adapt: internal review",
+        "type": "",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":30
+    },
+    {
+        "item": "Script for adapt: shared with client",
+        "type": "",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":31
+    },
+    {
+        "item": "Client sign off on script for adapt",
+        "type": "",
+        "client_action": true,
+        "important_action": false,
+        "heading":false,
+        "order":32
+    },
+    {
+        "item": "First draft of adapt: internal review",
+        "type": "Adapts",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":33
+    },
+    {
+        "item": "First draft of adapt: shared with client",
+        "type": "Adapts",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":34
+    },
+    {
+        "item": "Client review of first adapt draft",
+        "type": "Adapts",
+        "client_action": true,
+        "important_action": false,
+        "heading":false,
+        "order":35
+    },
+    {
+        "item": "Final draft of adapt: internal review",
+        "type": "Adapts",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":36
+    },
+    {
+        "item": "Final draft of adapt: shared with client",
+        "type": "Adapts",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":37
+    },
+    {
+        "item": "Client sign off on final adapt draft ",
+        "type": "Adapts",
+        "client_action": true,
+        "important_action": false,
+        "heading":false,
+        "order":38
+    },
+    {
+        "item": "Handover of complete film adapt video",
+        "type": "Adapts",
+        "client_action": false,
+        "important_action": false,
+        "heading":true,
+        "order":39
+    },
+    {
+        "item": "Handover of RAW files, Open Files and Creative Assets: Post the reciept of final payment",
+        "type": "Adapts",
+        "client_action": false,
+        "important_action": false,
+        "heading":false,
+        "order":40
+    },
+]
